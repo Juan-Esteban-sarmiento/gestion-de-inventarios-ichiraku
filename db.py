f794bbea5e1bc0f4f05504370db8a25f2a28a839
juan <jsarmiento84@uan.edu.co>
2025-09-02 07:59:29 +0200

    se centralizo y pulio el diseño de incio de administrador y registor de empleados, se contecta y prueba la base de datos, se integra el registro de empleados y la bsuqueda de empleados

--- db.py
+++ db.py
@@ -1,5 +1,6 @@
 import mysql.connector
 
+# función de conexión
 def get_db_connection():
     conn = mysql.connector.connect(
         host="localhost",
@@ -8,3 +9,23 @@ def get_db_connection():
         database="dbgich"
     )
     return conn
+
+def get_empleados():
+    conn = get_db_connection()
+    cursor = conn.cursor(dictionary=True) 
+    cursor.execute("SELECT * FROM empleados")
+    empleados = cursor.fetchall()
+    cursor.close()
+    conn.close()
+    return empleados
+
+
+def add_empleado(cedula, nombre, numero_contacto, contrasena, foto):
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    sql = "INSERT INTO empleados (Cedula, Nombre, Numero_contacto, Contrasena, Foto) VALUES (%s, %s, %s, %s, %s)"
+    values = (cedula, nombre, numero_contacto, contrasena, foto)
+    cursor.execute(sql, values)
+    conn.commit()
+    cursor.close()
+    conn.close()
