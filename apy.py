8850d996de2a3485ca957781f0ead5df64566c04
Fuad David Franco <ffranco14@uan.edu.co>
2025-09-08 20:52:49 -0500

    prueba base de datos

--- apy.py
+++ apy.py
@@ -6,6 +6,7 @@ app.secret_key = '123456789'
 
 #redirecion inicial de logueo
 
+
 @app.route('/')
 def index():
     return redirect(url_for('login'))
@@ -180,3 +181,4 @@ def logout():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
