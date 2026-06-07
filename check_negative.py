e750500c3e11103a1ba51c9f2b676e1c64365256
juan sarmiento <jsarmiento84@uan.edu.co>
2026-06-06 23:05:29 -0500

    refactor: remove auxiliary test scripts and redundant backup files while initializing project template structures and cache directories

--- check_negative.py
+++
@@ -1,9 +0,0 @@
-import os
-from supabase import create_client
-from dotenv import load_dotenv
-
-load_dotenv()
-supabase = create_client(os.getenv("SUPABASE_URL"), os.getenv("SUPABASE_KEY"))
-
-res = supabase.table("inventario").select("*").lt("cantidad", 0).execute()
-print("Negative items:", res.data)
